5c04cf643efa597b372f06f48ed2e0e1a74c3488
Behera Manav <manavbehera123@gmail.com>
2025-07-13 11:46:51 +0530

    refactor redis connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,11 +11,8 @@ const port = 5000;
 
 app.use(cors());
 app.use(cookieParser());
-const redisUrl = process.env.REDIS_PROD_URL ?? "localhost";
-const redis = new Redis({
-  host: config.redis_url ?? "localhost",
-  port: config.redis_port ?? 6379,
-});
+const redisUrl = config.redis_url ?? "localhost";
+const redis = new Redis(redisUrl);
 app.use(traceMiddleware);
 app.use("/auth", authRouter);
 
